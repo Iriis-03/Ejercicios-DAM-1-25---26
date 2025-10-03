a9404fe5b9f64a16b500dbd3e177476c17e3ff9b
Iriis-03 <iriis.workflow@gmail.com>
2025-10-03 19:36:39 +0200

    Prectica4.java

--- Ejercicios DAM 1/src/main/java/org/example/Prectica4.java
+++ Ejercicios DAM 1/src/main/java/org/example/Prectica4.java
@@ -3,28 +3,42 @@ package org.example;
 
 import java.util.Scanner;
 
-public class Prectica4 {
-
+public class Practica4 {
 
 
     public static void main() {
 
         Scanner read = new Scanner(System.in);
 
-        int multiplicando = 723;
-        int multiplicador = 112;
+        int multiplicando;
+        int multiplicador;
+        int number = 0;
+        int Resultado = 0;
+
+        System.out.println("Introduce el multiplicando (3 cifras)");
+        multiplicando = read.nextInt();
+
+        System.out.println("Introduce el multiplicador (3 cifras)");
+        multiplicador = read.nextInt();
 
         String number_string = Integer.toString(multiplicando);
 
-        String middle_number = number_string.substring(1,2);
+        String first_number = number_string.substring(0,1);
+        int first_number_int = Integer.parseInt (first_number);
 
+        String middle_number = number_string.substring(1,2);
         int middle_number_int = Integer.parseInt (middle_number);
 
-        System.out.println("Introduce el multiplicando, por favor...");
-        multiplicando = read.nextInt();
+        String last_number = number_string.substring(2,3);
+        int last_number_int = Integer.parseInt (last_number);
 
 
+        System.out.println("El producto de la multiplicación es " + Resultado);
+        Resultado = read.nextInt();
 
+        System.out.println("Resultado: " + first_number_int * number);
+        System.out.println("Resultado: " + middle_number_int * number);
+        System.out.println("Resultado: " + last_number_int * number);
 
       //  System.out.println("Resultado: " + middle_number_int*number);
     }
