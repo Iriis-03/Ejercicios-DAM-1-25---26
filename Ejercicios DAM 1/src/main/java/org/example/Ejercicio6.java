119a6dc8c5b4ad1a764a5a23706843721163b872
LAPTOP-K1RFIGDH\34637 <iriis.workflow@gmail.com>
2025-10-12 16:04:53 +0200

    Prueba práctica ejercicio6 finalizada

---
+++ Ejercicios DAM 1/src/main/java/org/example/Ejercicio6.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Ejercicio6 {
+
+    public static void main (){
+
+        Scanner read = new Scanner(System.in);
+
+        int base = 0;
+        int altura = 0;
+        double area;
+
+        do  {
+
+            System.out.println("Introduce el valor de la base del triángulo");
+            base = read.nextInt();
+
+            if (base <= 0)
+                System.out.println("El valor de la base tiene que ser positivo");
+
+        }while (base <= 0);
+
+        do  {
+
+            System.out.println("Introduce el valor de la altura del triángulo");
+            altura = read.nextInt();
+
+            if (altura <= 0)
+                System.out.println("El valor de la altura tiene que ser positivo");
+
+        }while (altura <= 0);
+
+        area = (base * altura) / 2;
+
+        System.out.println("El área del triángulo es " + area);
+    }
+
+        }
