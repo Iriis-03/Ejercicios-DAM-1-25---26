c9a71bff96099411dae4f3dcfea2695e15b8f0d1
LAPTOP-K1RFIGDH\34637 <iriis.workflow@gmail.com>
2025-10-16 09:56:45 +0200

    Continuación Práctica 1 TEMA 2

--- Ejercicios DAM 1/src/main/java/org/example/TEMA2/Practica1_TEMA2.java
+++ Ejercicios DAM 1/src/main/java/org/example/TEMA2/Practica1_TEMA2.java
@@ -11,23 +11,43 @@ public class Practica1_TEMA2 {
         System.out.println("Introduce tu fecha de nacimiento (dd/mm/aaaa");
         String fecha = read.nextLine();
 
-        String number_string = Integer.toString(Integer.parseInt(fecha));
+        if (fecha.charAt(2) != '/' || fecha.charAt(5) != '/') {
+            System.out.println("El formato no es correcto. Usa /");
+        } else {
 
-        String dia = number_string.substring (0,1);
-        int dia_int = Integer.parseInt(dia);
+            try {
+                String dia_string = fecha.substring(0, 2);
+                String mes_string = fecha.substring(3, 5);
+                String anyo_string = fecha.substring(6, 10);
 
-        String mes = number_string.substring (4,5);
-        int mes_int = Integer.parseInt(mes);
+                int dia_int = Integer.parseInt(dia_string);
+                int mes_int = Integer.parseInt(mes_string);
+                int anyo_int = Integer.parseInt(anyo_string);
 
-        String anyo = number_string.substring (7,10);
-        int anyo_int = Integer.parseInt(mes);
+                if (dia_int > 30 || dia_int < 1 ) {
+                    System.out.println("ERROR DE LECTURA. DÍA INCORRECTO");
+                }else if (mes_int > 12 || mes_int < 1) {
+                    System.out.println("ERROR DE LECTURA. MES INCORRECTO");
+                }else if (anyo_int > 2025 || anyo_int < 1900){
+                    System.out.println("ERROR DE LECTURA. AÑO INCORRECTO");
+                } else {
 
-        if (dia_int > 30 || dia_int < 1 ){
-            System.out.println("ERROR DE LECTURA EN EL DÍA");
-            if (mes_int > 12 || mes_int < 1){
-                System.out.println("ERROR DE LECTURA EN EL MES");
-                if (anyo_int > 2025 || anyo_int < 1799){
-                    System.out.println("ERROR DE LECTURA EN EL AÑO");
+                    int suma = dia_int + mes_int + anyo_int;
+                    System.out.println(dia_int + " + " + mes_int + " + " + anyo_int + " = " + suma);
+
+                    String suma_string = Integer.toString(suma);
+
+                    int cifra1 = Integer.parseInt(suma_string.substring(0, 1));
+                    int cifra2 = Integer.parseInt(suma_string.substring(1, 2));
+                    int cifra3 = Integer.parseInt(suma_string.substring(2, 3));
+                    int cifra4 = Integer.parseInt(suma_string.substring(3, 4));
+
+                    int resultado = cifra1 + cifra2 + cifra3 + cifra4;
+
+                    System.out.println (cifra1 + " + " + cifra2 + " + " + cifra3 + " + " + cifra4 + " = " + resultado);
+
+                    System.out.println("Tu número de la suerte es: " + resultado);
+                }
                 }
             }
         }
