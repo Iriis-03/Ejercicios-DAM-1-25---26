cb8e1d6b2e810171494a62c0fa865d39ee887ee8
A702\dam1 <iriis.workflow@gmail.com>
2025-10-21 15:33:49 +0200

    Actividad1 incorporando Switch

--- Ejercicios DAM 1/src/main/java/org/example/TEMA2/Actividad1.java
+++ Ejercicios DAM 1/src/main/java/org/example/TEMA2/Actividad1.java
@@ -1,12 +1,11 @@
 package org.example.TEMA2;
 
 import java.time.LocalDateTime;
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Actividad1 {
 
-    static void main() {
+     static void main() {
 
         Scanner read = new Scanner(System.in);
 
@@ -24,6 +23,7 @@ public class Actividad1 {
         System.out.println("[1] - Año de nacimiento");
         System.out.println("[2] - Edad");
         System.out.println("-----------------------");
+
         if (read.hasNextInt()) {
             modo = read.nextInt();
         } else {
@@ -33,63 +33,60 @@ public class Actividad1 {
 
         if (seguir) {
 
-            if (modo == 1) {
+            switch (modo) {
+                case 1:
+                    System.out.println("Introduce tu año de nacimiento: ");
+                    String anyo_nacimiento = read.next();
 
-                System.out.println("Introduce tu año de nacimiento: ");
-                String anyo_nacimiento = read.next();
+                    try {
+                        anyo_nacimiento_int = Integer.parseInt(anyo_nacimiento);
+                    } catch (NumberFormatException e) {
+                        System.out.println("El formato no es numérico.");
+                        return;
+                    }
+                    break;
 
-                try {
-                    anyo_nacimiento_int = Integer.parseInt(anyo_nacimiento);
-                } catch (NumberFormatException e) {
-                    System.out.println("El formato no es númerico.");
+                case 2:
+                    System.out.println("Introduce tu edad:");
+
+                    if (read.hasNextInt()) {
+                        edad = read.nextInt();
+                        if (edad >= 0) {
+                            anyo_nacimiento_int = anyo_actual - edad;
+                        } else {
+                            System.out.println("La edad no puede ser negativa.");
+                            return;
+                        }
+                    } else {
+                        System.out.println("La edad introducida no es válida.");
+                        return;
+                    }
+                    break;
+
+                default:
+                    System.out.println("El modo introducido no es correcto. (numérico)");
                     return;
-                }
-
-
-
-            }
-            else if(modo ==2)
-
-            {
-
-                System.out.println("Introduce tu edad:");
-
-                if (read.hasNextInt()) {
-                    edad = read.nextInt();
-                }
-
-            } else{
-                System.out.println("El modo introducido no es correcto. (númerico)");
-                return;
             }
 
-            if(edad >=0){
-                anyo_nacimiento_int = anyo_actual - edad;
-            } else
+            if (anyo_nacimiento_int >= ANYO_MINIMO && anyo_nacimiento_int <= anyo_actual) {
 
-            {
-                System.out.println("El modo introducido no es correcto");
-                return;
+                if (anyo_nacimiento_int <= 1927) {
+                    System.out.println("Generación sin bautizar.");
+                } else if (anyo_nacimiento_int <= 1944) {
+                    System.out.println("Generación silent");
+                } else if (anyo_nacimiento_int <= 1964) {
+                    System.out.println("Baby boomer");
+                } else if (anyo_nacimiento_int <= 1982) {
+                    System.out.println("Generación X");
+                } else if (anyo_nacimiento_int <= 1994) {
+                    System.out.println("Millenials la mejor");
+                } else {
+                    System.out.println("Generación Z");
+                }
 
-        }
-
-    }
-        if (anyo_nacimiento_int >= ANYO_MINIMO && anyo_nacimiento_int <= anyo_actual) {
-
-            if (anyo_nacimiento_int <= 1927) {
-                System.out.println("Generación sin bautizar.");
-            } else if (anyo_nacimiento_int >= 1928 && anyo_nacimiento_int <= 1944) {
-                System.out.println("Generación silent");
-            } else if (anyo_nacimiento_int >= 1945 && anyo_nacimiento_int <= 1964) {
-                System.out.println("Baby boomer");
-            } else if (anyo_nacimiento_int >= 1965 && anyo_nacimiento_int <= 1982) {
-                System.out.println("Generación X");
-            } else if (anyo_nacimiento_int >= 1982 && anyo_nacimiento_int <= 1994) {
-                System.out.println("Millenials la mejor");
             } else {
-                System.out.println("Generación Z");
+                System.out.println("Año de nacimiento fuera de rango.");
             }
-
         }
     }
 }
