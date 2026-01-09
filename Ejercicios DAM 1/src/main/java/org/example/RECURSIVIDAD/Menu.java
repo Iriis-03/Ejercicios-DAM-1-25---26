5baf8a55d95ac9e41092bff7d1c31396107f0fa5
LAPTOP-K1RFIGDH\34637 <iriis.workflow@gmail.com>
2026-01-09 19:31:29 +0100

    CLASE 09/01/2026

--- Ejercicios DAM 1/src/main/java/org/example/RECURSIVIDAD/Menu.java
+++ Ejercicios DAM 1/src/main/java/org/example/RECURSIVIDAD/Menu.java
@@ -9,8 +9,23 @@ public class Menu {
         Scanner read = new Scanner(System.in);
 
         menu();
-        System.out.println(dijitos(28383));
+        System.out.println(dijitos(4556));
         System.out.println(potencias(2, 4));
+        del_reves(25);
+
+        System.out.println();
+
+        String frase = "Hola mundo soy una frase al revés";
+        char[] frase_char = frase.toCharArray();
+        del_reves_char(frase_char.length - 1, frase_char);
+        System.out.println();
+        del_reves_char_sin_vector(frase.length() - 1, frase);
+
+        System.out.println();
+
+        System.out.println(binario(10001110));
+        System.out.println(a_binario(128));
+
 
     }
 
@@ -68,5 +83,67 @@ public class Menu {
             return base * potencias(base, exponente - 1);
         }
 
+    }
+
+    public static void del_reves(int num){
+
+        if (num < 10){
+            System.out.print(num);
+        } else {
+            System.out.print(num % 10);
+            del_reves(num/10);
+        }
+
+    }
+
+    public static void del_reves_char(int posicion, char[] frase){
+
+        if (posicion >= 0){
+            System.out.print(frase[posicion]);
+            del_reves_char(posicion - 1, frase);
+        }
+
+    }
+
+    public static void del_reves_char_sin_vector(int posicion, String frase){
+
+        if (posicion >= 0){
+            System.out.print(frase.charAt(posicion));
+            del_reves_char_sin_vector(posicion - 1, frase);
+        }
+
+    }
+
+    public static boolean binario (int num){
+
+        if (num >= 10){
+
+            if (num % 10 == 0 || num % 10 == 1){
+                return binario(num / 10);
+            } else {
+                return false;
+            }
+        } else {
+            if (num == 0 || num == 1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public static String a_binario(int num){
+
+        if (num <= 1){
+            return Integer.toString(num);
+        } else {
+            return a_binario(num / 2) + num % 2;
+        }
+
+    }
+
+    public static boolean orden_alfabetico(String palabra, int posicion){
+
+        if (palabra )
     }
 }
